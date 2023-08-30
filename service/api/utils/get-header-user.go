bd0a98c7ca850e0fbc1ccdbb4615acb8a6add94a
bibi2001 <bibiana.farinha@gmail.com>
2023-08-30 02:51:03 +0200

    initial commit

---
+++ service/api/utils/get-header-user.go
@@ -0,0 +1,22 @@
+package utils
+
+import{
+	"errors"
+	"net/http"
+	"strings"
+}
+
+func GetBearerToken(r *http.Request) (string, error) {
+    // Get the Authorization header
+    header := r.Header.Get("Authorization")
+
+    // Check if the Authorization header is empty or does not start with "Bearer "
+    if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+        return "", errors.New("Invalid Authorization header")
+    }
+
+    // Extract the user identifier (token) from the header
+    userIdentifier := strings.TrimPrefix(authHeader, "Bearer ")
+
+    return userIdentifier, nil
+}
