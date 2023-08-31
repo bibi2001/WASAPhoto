68e66c2662bcff768e22abee9a5999f631925583
bibi2001 <bibiana.farinha@gmail.com>
2023-08-31 03:08:48 +0200

    fix bugs

--- service/api/endpoints-likes.go
+++ service/api/endpoints-likes.go
@@ -1,35 +1,26 @@
 package api
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/bibi2001/WASAPhoto/service/api/reqcontext"
 	"github.com/julienschmidt/httprouter"
 )
 
-func (rt *_router) LikesEndpoints(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
-	// Read the username to be followed from the request body.
-	var photoId string
-	err := json.NewDecoder(r.Body).Decode(&photoId)
-	if err != nil {
-		// The body was not a parseable JSON, reject it
-		http.Error(w, "Invalid JSON Body", http.StatusBadRequest)
-		return
-	}
+func (rt *_router) LikeUnlike(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+	// Read the photoId from the parameters
+	photoId := ps.ByName("photoId")
+	authUser := ps.ByName("username")
 
+	// Check the validaty of the photoId parameter
 	photoExists, err := rt.db.PhotoExists(photoId)
 	if err != nil {
-		// In this case, we have an error on our side. Log the error (so we can be notified) and send a 500 to the user
-		// Note: we are using the "logger" inside the "ctx" (context) because the scope of this issue is the request.
-		ctx.Logger.WithError(err).Error("can't validate photoId")
+		ctx.Logger.WithError(err).Error("error searching for the photo")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	if !photoExists {
-		// Here we validate the Request Body photoId, and we
-		// discovered that it is not valid.
-		http.Error(w, "Invalid Username", http.StatusBadRequest)
+		http.Error(w, "Invalid photoID", http.StatusBadRequest)
 		return
 	}
 
@@ -40,15 +31,20 @@ func (rt *_router) LikesEndpoints(w http.ResponseWriter, r *http.Request, ps htt
 		return
 	}
 	// Get the username corresponding to the Token
-	authUser, err := rt.db.GetUsername(token)
+	authUsername, err := rt.db.GetUsername(token)
 	if err != nil {
 		ctx.Logger.WithError(err).Error("authenticated username can not be found")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+	if authUser != authUsername {
+		http.Error(w, "Authenticated userID doesn't match authenticated username",
+			http.StatusUnauthorized)
+		return
+	}
 
 	// Check if we should like or unlike the photo
-	if r.Method == http.MethodPost {
+	if r.Method == http.MethodPut {
 		// Like the photo
 		err = rt.db.LikePhoto(authUser, photoId)
 		if err != nil {
