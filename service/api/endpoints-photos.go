6240d7258bfcd7c8dac9e0a654348e96a2f26ebd
bibi2001 <bibiana.farinha@gmail.com>
2023-08-31 04:54:55 +0200

    add DeletePhoto function

--- service/api/endpoints-photos.go
+++ service/api/endpoints-photos.go
@@ -60,6 +60,48 @@ func (rt *_router) UploadPhoto(w http.ResponseWriter, r *http.Request, ps httpro
 	_ = json.NewEncoder(w).Encode(photo)
 }
 
+func (rt *_router) DeletePhoto(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
+	// Read the photoId from the parameters
+	photoId := ps.ByName("photoId")
+	// There is no need to validate the identifier as we already do that on the database respective file
+
+	// Get the Bearer Token in the header
+	token, err := GetBearerToken(r)
+	if err != nil {
+		http.Error(w, "Invalid Bearer Token", http.StatusUnauthorized)
+		return
+	}
+	// Get the username corresponding to the Token
+	authUser, err := rt.db.GetUsername(token)
+	if err != nil {
+		ctx.Logger.WithError(err).Error("authenticated username cannot be found")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Check if authenticated user is the owner of the photo
+	isPhotoOwner, err := rt.db.IsPhotoOwner(authUser, photoId)
+	if err != nil {
+		ctx.Logger.WithError(err).Error("could not get photo owner")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	if !isPhotoOwner {
+		// User isn't owner of the photo, so they can't delete it
+		http.Error(w, "Not the owner of the photo", http.StatusUnauthorized)
+		return
+	}
+
+	err = rt.db.DeletePhoto(photoId)
+	if err != nil {
+		ctx.Logger.WithError(err).Error("could not delete photo")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func (rt *_router) GetPhoto(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
 	// Read the photoId from the parameters
 	photoId := ps.ByName("photoId")
