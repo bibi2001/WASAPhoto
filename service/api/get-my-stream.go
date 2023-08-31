5854310b0a571321d60053900bb5d7bf89e6ac02
bibi2001 <bibiana.farinha@gmail.com>
2023-08-31 02:46:05 +0200

    initial commit

--- service/api/get-my-stream.go
+++ service/api/get-my-stream.go
@@ -2,37 +2,13 @@ package api
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/bibi2001/WASAPhoto/service/api/reqcontext"
 	"github.com/julienschmidt/httprouter"
 )
 
-func (rt *_router) UploadPhoto(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
-
-	// Get the file from the form
-	file, _, err := r.FormFile("image")
-	if err != nil {
-		http.Error(w, "Error opening file", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
-
-	// Read the image into a byte slice
-	imageBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		ctx.Logger.WithError(err).Error("failed reading the image file")
-		http.Error(w, "Error reading the file", http.StatusInternalServerError)
-		return
-	}
-
-	// Get the caption from the form
-	caption := r.FormValue("caption")
-	if !ValidateCaption(caption) {
-		http.Error(w, "Invalid caption format", http.StatusBadRequest)
-		return
-	}
+func (rt *_router) GetMyStream(w http.ResponseWriter, r *http.Request, ps httprouter.Params, ctx reqcontext.RequestContext) {
 
 	// Get the Bearer Token in the header
 	token, err := GetBearerToken(r)
@@ -44,21 +20,21 @@ func (rt *_router) UploadPhoto(w http.ResponseWriter, r *http.Request, ps httpro
 	// Get the username corresponding to the Token
 	authUser, err := rt.db.GetUsername(token)
 	if err != nil {
-		ctx.Logger.WithError(err).Error("authenticated username can not be found")
+		ctx.Logger.WithError(err).Error("authenticated username cannot be found")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	// Upload the photo
-	photo, err := rt.db.UploadPhoto(authUser, caption, imageBytes)
+	userStream, err := GetUserStream(authUser)
 	if err != nil {
-		ctx.Logger.WithError(err).Error("can't upload photo")
+		ctx.Logger.WithError(err).Error("could not get user stream")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
+	// Return user stream
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(userStream)
 
-	w.WriteHeader(http.StatusCreated)
-	_ = json.NewEncoder(w).Encode(photo)
 }
