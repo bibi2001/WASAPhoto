6a275ba59f1b68aa6d085b0e5fb4134b726917b1
bibi2001 <bibiana.farinha@gmail.com>
2023-09-01 16:42:32 +0200

    organize imports

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -28,17 +28,18 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/ardanlabs/conf"
 	"github.com/bibi2001/WASAPhoto/service/api"
 	"github.com/bibi2001/WASAPhoto/service/database"
 	"github.com/bibi2001/WASAPhoto/service/globaltime"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/sirupsen/logrus"
-	"math/rand"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 // main is the program entry point. The only purpose of this function is to call run() and set the exit code if there is
