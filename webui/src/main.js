603c17b56c16da4e076e2a7c42a9e131f265bac2
bibi2001 <bibiana.farinha@gmail.com>
2023-09-07 02:31:50 +0200

    Import components

--- webui/src/main.js
+++ webui/src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import axios from './services/axios.js';
 import ErrorMsg from './components/ErrorMsg.vue'
 import LoadingSpinner from './components/LoadingSpinner.vue'
+import UserList from './components/UserList.vue'
+import Photo from './components/Photo.vue'
+import Comment from './components/Comment.vue'
+import Modal from './components/Modal.vue'
 
 import './assets/dashboard.css'
 import './assets/main.css'
@@ -12,5 +16,9 @@ const app = createApp(App)
 app.config.globalProperties.$axios = axios;
 app.component("ErrorMsg", ErrorMsg);
 app.component("LoadingSpinner", LoadingSpinner);
+app.component("UserList", UserList);
+app.component("Photo", Photo);
+app.component("Comment", Comment);
+app.component("Modal", Modal);
 app.use(router)
 app.mount('#app')
