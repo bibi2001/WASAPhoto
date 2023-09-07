0bb5a0cbf03225509aed7114676ae3c04a8f4123
bibi2001 <bibiana.farinha@gmail.com>
2023-09-07 08:13:54 +0200

    add username checks

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -1,78 +1,67 @@
 <script>
-import { setAuthToken , getAuthToken, setAuthUsername, getAuthUsername} from '../services/tokenService';
+import { setAuthToken, getAuthToken, setAuthUsername } from '../services/tokenService';
 
 export default {
-	data: function () {
-		return {
-			errormsg: null,
-			loading: false,
-			
-			username: "",
-		}
-	},  
-	
-	methods: {
-		async login () {
-			this.loading = true;
-			this.errormsg = null;
-			try {
-				const response = await this.$axios.post("/session", {
-					name: this.username,
-				});
+  data: function () {
+    return {
+      errormsg: null,
+      loading: false,
+      username: '',
+    }
+  },
 
-				if (response.status == 201) {
-					setAuthToken(response.data["identifier"]);
-					setAuthUsername(this.username);
+  methods: {
+    async login() {
+      this.loading = true;
+      this.errormsg = null;
+      try {
+        if (this.username.length < 3 || this.username.length > 16) {
+          this.errormsg = "The username has to be between 3 and 16 characters long.";
+        } else {
+          const usernameRegex = /^[a-zA-Z0-9_]+$/;
+          if (!usernameRegex.test(this.username)) {
+            this.errormsg = "The username has too many weird characters.";
+          } else {
+            const response = await this.$axios.post("/session", {
+              name: this.username,
+            });
 
-					// Debug 
-					console.log("Received Authorization Header:", getAuthToken());
-					console.log("Received Username:", getAuthUsername());
+            setAuthToken(response.data["identifier"]);
+            setAuthUsername(this.username);
 
-					this.$router.push("/home");
-				}
-
-				if (response.status == 400) {
-
-					// Debug 
-					console.log("Received Authorization Header:", getAuthToken());
-					console.log("Received Username:", getAuthUsername());
-
-					this.$router.push("/");
-				}
-
-			} catch (e) {
-				if (response.status == 400)
-					this.errormsg = "The username has to be between 3 and 16 characters long"
-				else
-					this.errormsg = e.toString();
-			}
-			this.loading = false;
-		}
-	}
+            this.$router.push("/home");
+          }
+        }
+      } catch (e) {
+        this.errormsg = e.toString();
+      }
+      this.loading = false;
+    }
+  }
 }
 </script>
 
 <template>
-	<div>
-		<div
-			class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-			<h1 class="h2">Login</h1>
-		</div>
+  <div>
+    <div
+      class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <h1 class="h2">Login</h1>
+    </div>
 
-		<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
+    <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
 
-		<div class="mb-3">
-			<label for="description" class="form-label">Try to remember your username!</label>
-			<input type="string" class="form-control" id="username" v-model="username" placeholder="yourusername">
-		</div>
+    <div class="mb-3">
+      <label for="description" class="form-label">Try to remember your username!</label>
+      <input type="text" class="form-control" id="username" v-model="username" placeholder="yourusername">
+    </div>
 
-		<div>
-			<button v-if="!loading" type="button" class="btn btn-primary" @click="login">
-				Sign in
-			</button>
-			<LoadingSpinner v-if="loading"></LoadingSpinner>
-		</div>
-	</div>
+    <div>
+      <button v-if="!loading" type="button" class="btn btn-primary" @click="login">
+        Sign in
+      </button>
+      <LoadingSpinner v-if="loading"></LoadingSpinner>
+    </div>
+  </div>
 </template>
 
 <style>
