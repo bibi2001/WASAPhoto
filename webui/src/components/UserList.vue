c119398f27b310a2230904081f49ed9db6a1719d
bibi2001 <bibiana.farinha@gmail.com>
2023-09-07 02:32:03 +0200

    initial commit

---
+++ webui/src/components/UserList.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    props: ["username"],
+	data: function() {
+		return {
+			errormsg: null,
+			loading: false,
+		}
+	},
+    methods: {
+		async goToProfile() {
+			this.$router.push("/profile/"+this.username);
+		},
+    }, 
+}
+</script>
+
+<template>
+	<div class="card" @click="goToProfile" style="cursor: pointer;">
+	  <div class="card-body">
+		<p class="username">{{ username }}</p>
+	  </div>
+	</div>
+</template>
+  
+<style scoped>
+.card {
+	margin-bottom: 10px;
+	height:60px;
+}
+.username {
+	font-size: 18px;
+}
+</style>
